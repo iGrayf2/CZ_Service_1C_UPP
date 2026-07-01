c22e1688092f7cd659b1c46c9f3fde41974eac62
Sergey <scherbak.s1989@gmail.com>
2026-07-01 14:44:55 +0300

    add ping test

---
+++ tests/test_ping.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+def test_ping() -> None:
+    client = TestClient(app)
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json()["ok"] is True
